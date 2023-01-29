5465db5c6b8bcfe0cb30c57b4912ea567f6d967c
Jeremias <jnahuel46@gmail.com>
2023-01-29 09:28:00 -0300

    add new images

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import blueLogo from './assets/dc-blue.jpg'
 import './App.css'
 
 function App() {
@@ -9,10 +10,10 @@ function App() {
     <div className="App">
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="vite.svg" className="logo" alt="Vite logo" />
+          <img src="dc-black.jpg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={blueLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
